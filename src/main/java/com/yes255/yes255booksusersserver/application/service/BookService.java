9a9dd22d7606036d3d96ae14ded5cc152851efdf
wnstj1548 <wnstj0614@naver.com>
2024-06-17 13:40:53 +0900

    Book Service(CRUD) 구현

--- src/main/java/com/yes255/yes255booksusersserver/application/service/BookService.java
+++ src/main/java/com/yes255/yes255booksusersserver/application/service/BookService.java
@@ -3,7 +3,6 @@ package com.yes255.yes255booksusersserver.application.service;
 import com.yes255.yes255booksusersserver.presentation.dto.request.CreateBookRequest;
 import com.yes255.yes255booksusersserver.presentation.dto.request.UpdateBookRequest;
 import com.yes255.yes255booksusersserver.presentation.dto.response.BookResponse;
-import com.yes255.yes255booksusersserver.presentation.dto.response.CreateBookResponse;
 
 import java.util.List;
 
