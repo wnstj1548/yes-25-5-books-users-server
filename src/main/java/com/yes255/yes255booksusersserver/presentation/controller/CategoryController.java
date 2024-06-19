06334be8a6bcc5f00740afc6b8ef3e15fa72c1e4
wnstj1548 <wnstj0614@naver.com>
2024-06-19 23:56:21 +0900

    feature : "swagger를 사용한 API 명세서 작성"

--- src/main/java/com/yes255/yes255booksusersserver/presentation/controller/CategoryController.java
+++ src/main/java/com/yes255/yes255booksusersserver/presentation/controller/CategoryController.java
@@ -6,6 +6,9 @@ import com.yes255.yes255booksusersserver.common.exception.ValidationFailedExcept
 import com.yes255.yes255booksusersserver.presentation.dto.request.CreateCategoryRequest;
 import com.yes255.yes255booksusersserver.presentation.dto.request.UpdateCategoryRequest;
 import com.yes255.yes255booksusersserver.presentation.dto.response.CategoryResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ import java.util.List;
 /**
  * 카테고리 관련 작업을 처리하는 RestController 클래스.
  */
+@Tag(name = "카테고리 API", description = "카테고리 관리 API")
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
@@ -28,6 +32,7 @@ public class CategoryController {
      *
      * @return ResponseEntity<List<CategoryResponse>> 형식의 카테고리 목록
      */
+    @Operation(summary = "모든 카테고리 조회", description = "등록된 카테고리를 조회합니다.")
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryResponse>> findAll() {
         return ResponseEntity.ok(categoryService.findAllCategories());
@@ -39,6 +44,7 @@ public class CategoryController {
      * @param categoryId 조회할 카테고리의 ID
      * @return ResponseEntity<CategoryResponse> 형식의 특정 카테고리 정보
      */
+    @Operation(summary = "특정 카테고리 조회", description = "categoryId로 특정 카테고리를 조회합니다.")
     @GetMapping("/categories/{categoryId}")
     public ResponseEntity<CategoryResponse> find(@PathVariable("categoryId") Long categoryId) {
         return ResponseEntity.ok(categoryService.findCategory(categoryId));
@@ -49,6 +55,7 @@ public class CategoryController {
      *
      * @return ResponseEntity<List<CategoryResponse>> 형식의 루트 카테고리 목록
      */
+    @Operation(summary = "루트 카테고리(1단계) 조회", description = "모든 카테고리 중 1단계 카테고리를 조회합니다.")
     @GetMapping("/categories/root")
     public ResponseEntity<List<CategoryResponse>> findRoot() {
         return ResponseEntity.ok(categoryService.findRootCategories());
@@ -60,6 +67,7 @@ public class CategoryController {
      * @param parentId 부모 카테고리의 ID
      * @return ResponseEntity<List<CategoryResponse>> 형식의 부모 카테고리에 속한 자식 카테고리 목록
      */
+    @Operation(summary = "부모 카테고리로 특정 카테고리 조회", description = "parentId를 받아 하위 카테고리를 조회합니다.")
     @GetMapping("/categories/parent/{parentId}")
     public ResponseEntity<List<CategoryResponse>> findByParentCategoryId(@PathVariable("parentId") Long parentId) {
         return ResponseEntity.ok(categoryService.findCategoryByParentCategoryId(parentId));
@@ -73,6 +81,8 @@ public class CategoryController {
      * @return ResponseEntity<CategoryResponse> 형식의 생성된 카테고리 정보
      * @throws ValidationFailedException 요청에 유효성 검사 오류가 있는 경우 발생합니다.
      */
+    @Operation(summary = "새로운 카테고리 생성", description = "새로운 카테고리를 생성합니다.")
+    @Parameter(name = "request", description = "categoryName(카테고리 이름)을 포함합니다.")
     @PostMapping("/categories")
     public ResponseEntity<CategoryResponse> create(@RequestBody @Valid CreateCategoryRequest createCategoryRequest, BindingResult bindingResult) {
 
@@ -91,6 +101,8 @@ public class CategoryController {
      * @return ResponseEntity<CategoryResponse> 형식의 업데이트된 카테고리 정보
      * @throws ValidationFailedException 요청에 유효성 검사 오류가 있는 경우 발생합니다.
      */
+    @Operation(summary = "기존 카테고리 업데이트", description = "기존 카테고리를 업데이트합니다.")
+    @Parameter(name = "request", description = "categoryId(PK), categoryName(카테고리 이름)을 포함합니다.")
     @PutMapping("/categories")
     public ResponseEntity<CategoryResponse> update(@RequestBody @Valid UpdateCategoryRequest updateCategoryRequest, BindingResult bindingResult) {
 
@@ -107,6 +119,7 @@ public class CategoryController {
      * @param categoryId 삭제할 카테고리의 ID
      * @return 삭제 성공 여부를 나타내는 ResponseEntity
      */
+    @Operation(summary = "기존 카테고리 삭제", description = "categoryId로 기존 카테고리를 삭제합니다.")
     @DeleteMapping("/categories/{categoryId}")
     public ResponseEntity<Void> delete(@PathVariable("categoryId") Long categoryId) {
 
