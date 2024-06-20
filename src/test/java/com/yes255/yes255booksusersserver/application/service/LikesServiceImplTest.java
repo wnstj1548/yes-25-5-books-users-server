81199235a01f67a1ceccfdb8e19475e769df037b
Hyeok000 <hyok000207@gmail.com>
2024-06-20 15:48:46 +0900

    feat : test 유저 수정

--- src/test/java/com/yes255/yes255booksusersserver/application/service/LikesServiceImplTest.java
+++ src/test/java/com/yes255/yes255booksusersserver/application/service/LikesServiceImplTest.java
@@ -54,7 +54,6 @@ public class LikesServiceImplTest {
         // Mock data setup
         testUser = User.builder()
                 .userEmail("test@gmail.com")
-                .userGrade(new UserGrade(null, "grade",null))
                 .userState(new UserState(null, "statename"))
                 .userPhone("010-2341-2342")
                 .userName("test")
