c53cc8f0c69f084ba56f9d50841f9b225c835aeb
wnstj1548 <wnstj0614@naver.com>
2024-06-22 15:26:59 +0900

    refactor : "도서 업데이트 관련 리팩토링"

--- src/main/java/com/yes255/yes255booksusersserver/persistance/domain/Book.java
+++ src/main/java/com/yes255/yes255booksusersserver/persistance/domain/Book.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.math.BigDecimal;
 import java.util.Date;
