a0395f1a41fef40b3c44f206102e2a85b6a14b76
Hyeok000 <hyok000207@gmail.com>
2024-06-25 00:08:38 +0900

    feat : swagger 작성

--- src/main/java/com/yes255/yes255booksusersserver/presentation/controller/ProviderController.java
+++ src/main/java/com/yes255/yes255booksusersserver/presentation/controller/ProviderController.java
@@ -6,12 +6,15 @@ import com.yes255.yes255booksusersserver.presentation.dto.request.provider.Updat
 import com.yes255.yes255booksusersserver.presentation.dto.response.provider.CreateProviderResponse;
 import com.yes255.yes255booksusersserver.presentation.dto.response.provider.ProviderResponse;
 import com.yes255.yes255booksusersserver.presentation.dto.response.provider.UpdateProviderResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "제공자 API", description = "제공자 관련 API 입니다.")
 @RequestMapping("/admin/providers")
 @RequiredArgsConstructor
 @RestController
@@ -19,31 +22,31 @@ public class ProviderController {
 
     private final ProviderService providerService;
 
-    // 제공자 생성
+    @Operation(summary = "제공자 생성", description = "제공자를 생성합니다.")
     @PostMapping
     public ResponseEntity<CreateProviderResponse> createProvider(@RequestBody CreateProviderRequest providerRequest) {
         return ResponseEntity.ok(providerService.createProvider(providerRequest));
     }
 
-    // 제공자 수정
+    @Operation(summary = "제공자 수정", description = "특정 제공자를 수정합니다.")
     @PutMapping("/{providerId}")
     public ResponseEntity<UpdateProviderResponse> updateProvider(@PathVariable Long providerId, @RequestBody UpdateProviderRequest providerRequest) {
         return ResponseEntity.ok(providerService.updateProvider(providerId, providerRequest));
     }
 
-    // 특정 제공자 조회
+    @Operation(summary = "제공자 조회", description = "특정 제공자를 조회합니다.")
     @GetMapping("/{providerId}")
     public ResponseEntity<ProviderResponse> getProviderById(@PathVariable Long providerId) {
         return ResponseEntity.ok(providerService.findProviderById(providerId));
     }
 
-    // 제공자 목록 조회
+    @Operation(summary = "제공자 목록 조회", description = "모든 제공자 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<List<ProviderResponse>> getAllProviders() {
         return ResponseEntity.ok(providerService.findAllProviders());
     }
 
-    // 제공자 삭제
+    @Operation(summary = "제공자 삭제", description = "특정 제공자를 삭제합니다.")
     @DeleteMapping("/{providerId}")
     public ResponseEntity<Void> deleteProvider(@PathVariable Long providerId) {
 
