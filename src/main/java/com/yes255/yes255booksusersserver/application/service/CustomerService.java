418f8e3d2a7835de8ef0b46dc6f7e75d10ae057d
Hyeok000 <hyok000207@gmail.com>
2024-06-25 10:44:04 +0900

    feat : 고객 기능 수정

--- src/main/java/com/yes255/yes255booksusersserver/application/service/CustomerService.java
+++ src/main/java/com/yes255/yes255booksusersserver/application/service/CustomerService.java
@@ -7,9 +7,14 @@ import com.yes255.yes255booksusersserver.presentation.dto.response.customer.Cust
 import java.util.List;
 
 public interface CustomerService {
+
     CustomerResponse createCustomer(CustomerRequest customerRequest);
+
     CustomerResponse updateCustomer(Long customerId, CustomerRequest customerRequest);
+
     CustomerResponse getCustomerById(Long customerId);
+
     List<CustomerResponse> getAllCustomers();
+
     void deleteCustomer(Long customerId);
 }
