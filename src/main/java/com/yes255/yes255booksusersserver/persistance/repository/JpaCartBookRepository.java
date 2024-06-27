f2328b881f7dae42e0c2d920eb6503048f1fac93
wnstj1548 <wnstj0614@naver.com>
2024-06-27 14:38:01 +0900

    feat : " entity 수정으로 인한 전체 코드 수정 (작가 객체 분리) "

--- src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaCartBookRepository.java
+++ src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaCartBookRepository.java
@@ -1,5 +1,6 @@
 package com.yes255.yes255booksusersserver.persistance.repository;
 
+import com.yes255.yes255booksusersserver.persistance.domain.Book;
 import com.yes255.yes255booksusersserver.persistance.domain.Cart;
 import com.yes255.yes255booksusersserver.persistance.domain.CartBook;
 import org.springframework.data.jpa.repository.JpaRepository;
