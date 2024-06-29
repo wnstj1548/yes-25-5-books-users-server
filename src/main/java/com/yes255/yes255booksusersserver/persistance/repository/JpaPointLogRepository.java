9eda8f2cebedc3d40f527751c2bc7edbbe472ee6
Hyeok000 <hyok000207@gmail.com>
2024-06-29 18:18:16 +0900

    feat : 포인트 이력 list -> page로 반화 수정

--- src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaPointLogRepository.java
+++ src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaPointLogRepository.java
@@ -1,6 +1,7 @@
 package com.yes255.yes255booksusersserver.persistance.repository;
 
 import com.yes255.yes255booksusersserver.persistance.domain.PointLog;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface JpaPointLogRepository extends JpaRepository<PointLog, Long> {
 
-    List<PointLog> findByPoint_User_UserIdOrderByPointLogUpdatedAtDesc(Long userId, Pageable pageable);
+    Page<PointLog> findByPoint_User_UserIdOrderByPointLogUpdatedAtDesc(Long userId, Pageable pageable);
 
     void deleteByPointUserUserId(Long userId);
 }
