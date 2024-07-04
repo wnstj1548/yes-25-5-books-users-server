aa15fe928d6e0bc3d80ef78ad267bc0346106b0e
Hyeok000 <hyok000207@gmail.com>
2024-07-04 18:10:56 +0900

    feat : 주문 서버로부터 쿠폰 사용 처리 기능 추가

--- src/main/java/com/yes255/yes255booksusersserver/presentation/controller/CouponUserController.java
+++ src/main/java/com/yes255/yes255booksusersserver/presentation/controller/CouponUserController.java
@@ -4,6 +4,7 @@ import com.yes255.yes255booksusersserver.application.service.CouponUserService;
 import com.yes255.yes255booksusersserver.common.jwt.JwtUserDetails;
 import com.yes255.yes255booksusersserver.common.jwt.annotation.CurrentUser;
 import com.yes255.yes255booksusersserver.persistance.domain.CouponUser;
+import com.yes255.yes255booksusersserver.presentation.dto.request.couponuser.UpdateCouponRequest;
 import com.yes255.yes255booksusersserver.presentation.dto.response.couponuser.CouponBoxResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,12 +15,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users/coupons")
+@RequestMapping("/users")
 public class CouponUserController {
 
     private final CouponUserService couponUserService;
 
-    @PostMapping("/claim")
+    @PostMapping("/coupons/claim")
     public ResponseEntity<Void> claimCoupon(@RequestParam Long couponId,
                      @CurrentUser JwtUserDetails jwtUserDetails) {
 
@@ -30,40 +31,25 @@ public class CouponUserController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/state")
+    @GetMapping("/coupons/state")
     public ResponseEntity<Page<CouponBoxResponse>> getActiveCouponBox(@RequestParam String couponState,
                                                                       Pageable pageable,
                                                                       @CurrentUser JwtUserDetails jwtUserDetails) {
-        CouponUser.UserCouponStatus status = CouponUser.UserCouponStatus.valueOf(couponState.toUpperCase());
+
         Long userId = jwtUserDetails.userId();
+        CouponUser.UserCouponStatus status = CouponUser.UserCouponStatus.valueOf(couponState.toUpperCase());
 
         return ResponseEntity.ok(couponUserService.getStateUserCoupons(userId, status, pageable));
     }
 
-//    @GetMapping("/active")
-//    public ResponseEntity<Page<CouponBoxResponse>> getActiveCouponBox(Pageable pageable,
-//                                                               @CurrentUser JwtUserDetails jwtUserDetails) {
-//
-//        Long userId = jwtUserDetails.userId();
-//
-//        return ResponseEntity.ok(couponUserService.getStateUserCoupons(userId, CouponUser.UserCouponStatus.ACTIVE, pageable));
-//    }
-//
-//    @GetMapping("/used")
-//    public ResponseEntity<Page<CouponBoxResponse>> getUsedCouponBox(Pageable pageable,
-//                                                                @CurrentUser JwtUserDetails jwtUserDetails) {
-//
-//        Long userId = jwtUserDetails.userId();
-//
-//        return ResponseEntity.ok(couponUserService.getStateUserCoupons(userId, CouponUser.UserCouponStatus.USED, pageable));
-//    }
-//
-//    @GetMapping("/expired")
-//    public ResponseEntity<Page<CouponBoxResponse>> getExpiredUsedCouponBox(Pageable pageable,
-//                                                                    @CurrentUser JwtUserDetails jwtUserDetails) {
-//
-//        Long userId = jwtUserDetails.userId();
-//
-//        return ResponseEntity.ok(couponUserService.getStateUserCoupons(userId, CouponUser.UserCouponStatus.EXPIRED, pageable));
-//    }
+    @PatchMapping("/user-coupons")
+    public ResponseEntity<Void> updateCouponState(@RequestBody UpdateCouponRequest couponRequest,
+                                                  @CurrentUser JwtUserDetails jwtUserDetails) {
+
+        Long userId = jwtUserDetails.userId();
+
+        couponUserService.updateCouponState(userId, couponRequest);
+
+        return ResponseEntity.ok().build();
+    }
 }
