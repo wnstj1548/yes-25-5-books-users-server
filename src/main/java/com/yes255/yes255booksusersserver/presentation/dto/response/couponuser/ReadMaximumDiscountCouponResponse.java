49f665b79b684af7b140eaddafd758f66b7f2330
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-12 09:16:20 +0900

    feat : 가장 할인율이 높은 쿠폰의 이름 반환값으로 추가함

--- src/main/java/com/yes255/yes255booksusersserver/presentation/dto/response/couponuser/ReadMaximumDiscountCouponResponse.java
+++ src/main/java/com/yes255/yes255booksusersserver/presentation/dto/response/couponuser/ReadMaximumDiscountCouponResponse.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 @Builder
-public record ReadMaximumDiscountCouponResponse(Long couponId, BigDecimal discountAmount) {
+public record ReadMaximumDiscountCouponResponse(Long couponId, BigDecimal discountAmount, String couponName) {
 }
