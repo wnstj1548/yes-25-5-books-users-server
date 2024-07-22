76e0e24cd4fd797a855e7f970076657344978ec4
wnstj1548 <wnstj0614@naver.com>
2024-07-22 19:58:53 +0900

    feat : "elastic search 업데이트 된 항목만 동기화하도록 변경"

--- src/main/java/com/yes255/yes255booksusersserver/BooksApiApplication.java
+++ src/main/java/com/yes255/yes255booksusersserver/BooksApiApplication.java
@@ -3,11 +3,13 @@ package com.yes255.yes255booksusersserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableJpaAuditing
 public class BooksApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaBookRepository.java
+++ src/main/java/com/yes255/yes255booksusersserver/persistance/repository/JpaBookRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +15,6 @@ public interface JpaBookRepository extends JpaRepository<Book, Long> {
     List<Book> findByBookIsDeletedFalse();
     List<Book> findByBookIsDeletedTrue();
     Optional<Book> findByBookIsbn(String isbn);
+    List<Book> findByBookIsDeletedFalseAndLastModifiedAfter(LocalDateTime lastModified);
+    List<Book> findByBookIsDeletedTrueAndLastModifiedAfter(LocalDateTime lastModified);
 }
